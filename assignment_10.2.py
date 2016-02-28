2657a4f7e8b425d9fc416e3d243abc289b068ccd
Tony Tian <tonytian89@126.com>
2016-02-27 19:33:53 -0500

    Update assignment_10.2.py

--- assignment_10.2.py
+++ assignment_10.2.py
@@ -2,10 +2,29 @@ name = raw_input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
 
-time = list()
+times = list()
+segs = list
+hrs = list()
+counts = dict()
 
 for line in handle:
     line = line.rstrip()
     if not line.startswith('From '): continue
     words = line.split()
-    time.append(words[5])
+    times.append(words[5])
+for time in times:
+    segs = time.split(":")
+    hrs.append(segs[0])
+    
+    
+for hr in hrs:
+    counts[hr] = counts.get(hr, 0) + 1
+    
+lst = list()
+for hr, fq in counts.items():
+    lst.append((hr, fq))
+    
+lst.sort()
+
+for hr, fq in lst:
+    print hr, fq
